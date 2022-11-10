09693a6ddbb767646ec410e53ec32963d7a48d3b
Neil <neilrowland56@yahoo.com>
2022-11-10 16:57:53 +0000

    Added layout for login and register

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,27 @@
 import './globals.css'
 
+import { Manrope } from '@next/font/google'
+
+
+const manrope = Manrope()
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={manrope.className}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className='  ' >
+        <main className=''>
+          
+        {children}
+        
+        </main>
+      </body>
+      
+      
     </html>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -2,16 +2,16 @@
 
 export default function Home() {
   return (
-    <div >
-      <main >
-        <h1 className="text-4xl font-bold text-red-600">
-          Welcome to Next Auth
+    < >
+      <main className="mt-5 ml-10" >
+        <h1 className='text-4xl font-bold text-red-500'>
+          Welcome to Homepage
         </h1>
 
         
       </main>
 
       
-    </div>
+    </>
   )
 }

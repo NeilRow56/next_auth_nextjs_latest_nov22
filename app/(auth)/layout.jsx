7b3f097c2e9dd7a0062ad05d3211cb485830dc2f
Neil <neilrowland56@yahoo.com>
2022-11-10 17:52:01 +0000

    Added images and animation to layout for login and register forms

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,16 +1,16 @@
-
+import styles from './layout.module.css';
 
 export default function ShopLayout({ children }) {
-  return (
-    
-      
+  return (     
      
       
          <section className='flex  h-screen bg-blue-400' >
         <div className="m-auto bg-slate-50 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2">
-          <div>
-            Images
-          </div>
+          <div className={styles.imgStyle}>
+                    <div className={styles.cartoonImg}></div>
+                    <div className={styles.cloud_one}></div>
+                    <div className={styles.cloud_two}></div>
+                </div>
           <div className="right flex flex-col justify-evenly bg-gray-500">
             <div className="text-center py-10">
               {children}
@@ -18,8 +18,7 @@ export default function ShopLayout({ children }) {
 
           </div>
 
-        </div>
-        
+        </div>  
         
       
       </section>
